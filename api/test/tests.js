8f56c904992c79b01da9f3d30fd31153529229a3
Kuliczkowsky <kulicz.pl@gmail.com>
2019-02-10 23:49:48 +0100

    First back-end tests, prepared Dockerfile for testing scripts

--- api/test/tests.js
+++ api/test/tests.js
@@ -1,1 +1,28 @@
 // TODO some tesitng
+let Spending = require('../models/spendingModel');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+var should = chai.should();
+let app = require('../index');
+require('sinon-mongoose');
+chai.use(chaiHttp);
+
+describe('Spendings API tests', () => {
+    beforeEach((done) => {
+        Spending.deleteMany({}, (err) => { 
+           done();           
+        });        
+    });
+    describe('/GET spendings', () => {
+        it('it should get the empty array of spendings', (done) => {
+          chai.request(app)
+              .get('/spendings')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.data.should.be.a('array');
+                    res.body.data.length.should.be.eql(0);
+                done();
+              });
+        });
+    });
+});
